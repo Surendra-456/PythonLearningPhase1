08e58f9156e7d7f5b75b21a22b03779334b4d88b
Surendra <snishad@infinite.com>
2026-08-13 19:07:32 +0545

    Disctionery complete

--- Disctionery/dictionery.py
+++ Disctionery/dictionery.py
@@ -7,3 +7,32 @@ print(student)
 print(student['name'])
 #what if key not found use get('key','optionalmessagewhen not exist') default=None
 print(student.get('name1','Not Found'))
+
+#update the value in  dictionery
+student.update({'name':'Surendra Pd Nishad','age':35, 'courses':['Math','Science','Nepali','Biology']})
+print(student)
+
+#del keyword to delete the specific key
+del student['courses']
+print(student)
+
+#another way to delete is pop() which will return delete key
+s=student.pop('name')
+print(student)
+print(s)
+
+#to see how many key use len(variable)
+print(len(student))
+
+#to see  keys use student.keys()
+print(student.keys())
+
+#to see  values use student.values()
+print(student.values())
+
+#to see  key and value use student.items()
+print(student.items())
+
+#for loop is just like list
+for key,value in student.items():
+    print(key,value)
